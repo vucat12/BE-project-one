f23c8bb6e517ff63014fd61c3d6d2f5cd158353d
hieunbk <nguyenhieu2542000@gmail.com>
2021-05-26 11:40:29 +0700

    update Lessor

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 var seller = require('./getSeller/seller.js')
 var buyer = require('./getBuyer/buyer.js')
+var lessor = require('./getLessor/lessor.js')
 var dataApp = require('./export-sever/exportSever.js')
 const port = 3002;
 
 MongoClient.connect(url, function(err, db) {
 if (err) throw err;
-var dbo = db.db("Project-1");
+var dbo = db.db("DoAn1");
 
 dbo.collection("CanBan").find({}).toArray((err, res) => seller.getSeller(err,res));
 dbo.collection("CanMua").find({}).toArray((err, res) => buyer.getBuyer(err,res));
+dbo.collection("ChoThue").find({}).toArray((err, res) => lessor.getLessor(err,res));
 
 const server = dataApp.listen(port, (error) => {
     if (error) return console.log(`Error: ${error}`);
